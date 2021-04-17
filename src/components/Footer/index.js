3419ae20a4894b7cd74cc4efb7e73e10ef45d4fd
HHSNZ <hh.stobo@gmail.com>
2021-04-17 15:14:45 +1200

    footer lookin gooood

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const Footer = () => {
     return (
-        <section className="bg-gray-700 text-center text-white p-8 py-20 w-full">
-            Hello from footer
+        <section className="bg-gray-700 text-center text-white py-4 w-full">
+            <div className="flex flex-row justify-center align-center py-4 text-3xl">
+                <a className="px-4" href="https://github.com/hamish-stobo" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
+                <a className="px-4" href="https://www.linkedin.com/in/hamishstobo" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
+                <a className="px-4" href="https://twitter.com/hamishhs1" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faTwitter} /></a>
+            </div>
+            <p>© {new Date().getFullYear()} Hamish Stobo</p>
         </section>
     )
 }
