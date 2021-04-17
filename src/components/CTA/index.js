07d1999487e49f69fc06c52139c9b70f1dab8a2a
HHSNZ <hh.stobo@gmail.com>
2021-04-17 12:11:32 +1200

    CTA v1 done, working on footer

--- src/components/CTA/index.js
+++ src/components/CTA/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const CTA = () => {
     return (
-        <section className="text-white flex flex-col justify-center align-center mt-4 bg-blue-400 pt-20 pb-32 sm:pb-40 CTAclippedRect">
+        <section className="text-white flex flex-col justify-between align-center text-center bg-blue-400 pt-8 sm:pt-20 px-4 pb-24 CTAclippedRect">
             <h2 className="text-3xl pt-4 sm:pt-0">Contact</h2>
-            <p>Click the button to send me an email. I'd love to hear from you!</p>
-            <a href="mailto:hh.stobo@gmail.com" title="hh.stobo@gmail.com">Let's Talk</a>
+            <p className="mt-12 text-lg sm:text-xl">Click the button to send me an email. I'd love to hear from you!</p>
+            <a className="mt-8 sm:mt-12 text-white mx-auto w-40 py-3 px-7 border-solid border-4 border-white text-xl" href="mailto:hh.stobo@gmail.com" title="hh.stobo@gmail.com">Let's Talk</a>
         </section>
     )
 }
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className="text-center w-full">
+        <section className="bg-gray-700 text-center text-white p-8 py-20 w-full">
             Hello from footer
-        </div>
+        </section>
     )
 }
 
