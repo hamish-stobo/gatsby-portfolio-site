d87f0ff80dd183a91e589ee37e474c8241f1e90f
HHSNZ <hh.stobo@gmail.com>
2021-05-08 14:11:19 +1200

    added form to website

--- src/components/CTA/index.js
+++ src/components/CTA/index.js
@@ -8,10 +8,15 @@ const CTA = ({ isTouchscreen }) => {
                 <h2 className="text-3xl pt-4 sm:pt-0">Contact</h2>
             </Fade>
             <Fade bottom duration={800} delay={150} distance="20px">
-                <p className="mt-12 text-lg sm:text-xl">Click the button to send me an email. I'd love to hear from you!</p>
+                <p className="mt-8 text-lg sm:text-xl">Send me a message - I'd love to hear from you!</p>
             </Fade>
             <Fade bottom duration={800} delay={180} distance="20px">
-                <a className={`mt-8 sm:mt-12 text-white mx-auto w-40 py-3 px-7 border-solid border-4 border-white cursor-pointer text-xl CTAbtn" href="mailto:hh.stobo@gmail.com ${!isTouchscreen ? 'CTAbtn' : ''}`} title="hh.stobo@gmail.com">Let's Talk</a>
+                <form method="POST" data-netlify="true" data-netlify-recaptcha="true" className="flex flex-col justify-center align-center text-center w-full max-w-2xl mt-8 h-80 mx-auto text-lg">
+                    <input className="text-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent rounded text-left py-2 px-4" type="email" placeholder="Your email"/>
+                    <textarea className="text-gray-600 h-40 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent rounded text-left py-2 px-4 sm:mt-4" placeholder="Your message" />
+                    <input type="submit" value="Let's Talk" className={`mt-6 text-white mx-auto w-40 py-3 px-7 border-solid border-4 border-white cursor-pointer text-xl bg-blue-400 ${!isTouchscreen ? 'CTAbtn' : ''}`} />
+                </form>
+                {/* <a className={`mt-4 sm:mt-12 text-white mx-auto w-40 py-3 px-7 border-solid border-4 border-white cursor-pointer text-xl CTAbtn" href="mailto:hh.stobo@gmail.com ${!isTouchscreen ? 'CTAbtn' : ''}`} title="hh.stobo@gmail.com">Let's Talk</a> */}
             </Fade>
         </section>
     )
