d33a9336f221e6934f50fe5944a81a0878c17f3a
HHSNZ <hh.stobo@gmail.com>
2021-05-08 14:49:27 +1200

    adding honeypot for bots

--- src/components/CTA/index.js
+++ src/components/CTA/index.js
@@ -11,13 +11,44 @@ const CTA = ({ isTouchscreen }) => {
                 <p className="mt-8 text-lg sm:text-xl">Send me a message - I'd love to hear from you!</p>
             </Fade>
             <Fade bottom duration={800} delay={180} distance="20px">
-                <form onSubmit="submit" name="contact" method="POST" data-netlify="true" data-netlify-recaptcha="true" className="flex flex-col justify-center align-center text-center w-full max-w-2xl mt-8 h-80 mx-auto text-lg">
-                    <input type="hidden" name="form-name" value="contact"></input>
-                    <label htmlFor="email" className="self-start" >Email address</label>
-                    <input id="email" autocomplete="email" required className="text-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent rounded text-left py-2 px-4" type="email" name="email"/>
-                    <label for="message" className="self-start mt-2" >Your message</label>
-                    <textarea required id="message" name="message" className="text-gray-600 h-40 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent rounded text-left py-2 px-4" />
-                    <button type="submit" className={`mt-6 text-white mx-auto w-40 py-3 px-7 border-solid border-4 border-white cursor-pointer text-xl bg-blue-400 ${!isTouchscreen ? 'CTAbtn' : ''}`}>Let's Talk</button>
+                <form 
+                    name="contact" 
+                    method="POST" 
+                    data-netlify="true"
+                    data-netlify-honeypot="bot-field"
+                    className="flex flex-col justify-center align-center text-center w-full max-w-2xl mt-8 h-80 mx-auto text-lg">
+                    <input 
+                        type="hidden" 
+                        name="form-name" 
+                        value="contact" 
+                    />
+                    <div hidden><input name="bot-field" /></div>
+                    <label htmlFor="email" className="self-start mb-1 pl-1" >Email address</label>
+                    <input 
+                        id="email" 
+                        autocomplete="email" 
+                        required 
+                        className="text-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent rounded text-left py-2 px-4" 
+                        type="email" 
+                        name="email"
+                    />
+                    <label 
+                        htmlFor="message" 
+                        className="self-start mt-4 mb-1 pl-1" 
+                    >
+                            Your message
+                    </label>
+                    <textarea 
+                        required 
+                        id="message" 
+                        name="message" 
+                        className="text-gray-600 h-40 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent rounded text-left py-2 px-4" 
+                    />
+                    <button 
+                        type="submit" 
+                        className={`focus:outline-none mt-6 text-white mx-auto w-40 py-3 px-7 border-solid border-4 border-white cursor-pointer text-xl bg-blue-400 ${!isTouchscreen ? 'CTAbtn' : ''}`}>
+                            Let's Talk
+                    </button>
                 </form>
             </Fade>
         </section>
